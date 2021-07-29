9b1caa01ee544988c805695ab66739f5a5813e3a
Jerson Carin <jersoncarin25@gmail.com>
2021-07-29 22:40:03 +0800

    Add server

--- index.js
+++ index.js
@@ -1,75 +1,15 @@
-const puppeteer = require('puppeteer');
-const fetch = require('node-fetch');
+const send = require('./send')
+const express = require('express')
 
-// Load dotenv
-require('dotenv').config();
+const app = express()
+const port = 3000
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+app.get('/v1/send', async (req, res) => {
+    await send()
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ status: 200 }));
+})
 
-  const page = await browser.newPage();
-
-  await (async () => {
-    // login
-    await page.goto('https://mbasic.facebook.com', {
-      waitUntil: 'networkidle2',
-      timeout: 0
-    });
-
-    console.log("Logging in...");
-
-    // Ensure that page is loaded and has login inputs
-    await page.waitForSelector('#m_login_email');
-
-    await page.type('#m_login_email', process.env.FB_USER);
-    await page.type('input[name="pass"]', process.env.FB_PASS);
-    await page.click("input[name='login']");
-
-    console.log("Sucessfully login");
-  })();
-
-  console.log('Finding given MSG_URL...');
-
-  await page.goto(process.env.FB_MSG_URL,{
-    waitUntil: 'networkidle2',
-    timeout: 0
-  });
-
-  console.log('MSG_URL has been successfully find');
-  console.log('Waiting for message...');
-  
-  setInterval(async () => {
-    const input = await page.$('#composerInput');
-
-    console.log("Getting random quotes...");
-
-    let quote = await fetch('https://zenquotes.io/api/random')
-      .then((res) => res.json())
-      .then((res) => res.shift())
-      .then((res) => res.q)
-      .then((res) => {
-        console.log("Random quotes has been fetch.");
-        return res
-      });
-
-    console.log("Sending message...");
-
-    if(!quote)
-      quote = 'hello world'
-  
-    await page.evaluate(async (input,quote) => {
-      const today  = new Date();
-      input.value = `\`${quote}\` \n\nSent via automated cron job, please don't reply \nTime: ${today.toLocaleString()}`
-      return input.value
-    },input,quote);
-
-    await page.click('input[name="send"]');
-
-    console.log(`Message has been sent\n`);
-
-  },process.env.TIME_DELAY || 300000);
-
-})();
+app.listen(port, () => {
+  console.log(`Random quotes app listening at http://localhost:${port}`)
+})
